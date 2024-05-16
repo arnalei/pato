61853b688c6ef685fc1353acf13b0badfcbbc306
Justin101602 <justinacosta832@gmail.com>
2024-05-16 22:32:31 +0800

    del

--- ICES2/ICES_Database/ICESdbo/ICESTable/ICESComments.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[ICESComments]
-(
-	[ICES] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [ICESIDNO] NVARCHAR(50) NULL, 
-    [ICESName] NVARCHAR(50) NULL, 
-    [ICESInstitutionalEmail] NVARCHAR(50) NULL, 
-    [ICESComment] NVARCHAR(50) NULL
-)
