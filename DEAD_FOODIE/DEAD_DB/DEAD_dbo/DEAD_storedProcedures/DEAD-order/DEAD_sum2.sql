0d7a6d34ff4b5ef709f6350dd1a41aa35ebdf766
Justin101602 <justinacosta832@gmail.com>
2024-05-16 21:01:10 +0800

    add

---
+++ DEAD_FOODIE/DEAD_DB/DEAD_dbo/DEAD_storedProcedures/DEAD-order/DEAD_sum2.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[DEAD_sum2]
+
+AS
+begin
+
+select 
+
+sum(DEAD_Price) as DEAD_Price
+from [DEAD_order]
+
+
+
+end
---
+++ DEAD_FOODIE/DEAD_DB/DEAD_dbo/DEAD_storedProcedures/DEAD-tb/DEAD_del.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[DEAD_del]
+	@DEAD_Id nvarchar(50)
+
+AS
+begin
+
+delete
+from [DEAD_TABLE]
+where DEAD_Id=@DEAD_Id
+
+
+
+end
