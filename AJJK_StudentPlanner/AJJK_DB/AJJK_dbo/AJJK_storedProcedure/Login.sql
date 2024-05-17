c84153414be13aee8dcb68f0d5e5daae17dee713
Justin101602 <justinacosta832@gmail.com>
2024-05-17 15:04:42 +0800

    del

--- AJJK_StudentPlanner/AJJK_DB/AJJK_dbo/AJJK_storedProcedure/Login.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE PROCEDURE [dbo].[Login]
-	@username NVARCHAR(50),
-    @password NVARCHAR(50)
-AS
-	BEGIN
-    SELECT [D].[username] AS username, [D].[password] AS password
-    FROM [dbo].[accounts] AS D
-    WHERE [D].[username] = @username AND [D].[password] = @password
-END;
