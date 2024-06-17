bc89d1950611df0688d4ba49cfcfdcf42ac798ef
Justin101602 <justinacosta832@gmail.com>
2024-06-17 14:22:52 +0800

    DEL

--- DEAD_FOODIE/DEAD_DB/DEAD_dbo/DEAD_table/DEAD_order.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[DEAD_order]
-(
-	[DEAD_Id] NVARCHAR(50) NOT NULL , 
-    [DEAD_Name] NVARCHAR(50) NOT NULL, 
-    [DEAD_Price] MONEY NOT NULL, 
-    [DEAD_duplicate]  INT identity
-    PRIMARY KEY ([DEAD_duplicate])
-)
