9b58100315207cc9f368337cad64e1972c693e0f
Justin101602 <justinacosta832@gmail.com>
2024-06-17 13:56:27 +0800

    add

---
+++ DEAD_FOODIE/DEAD_DB/DEAD_dbo/DEAD_storedProcedures/DEAD-tb/DEAD_add.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[DEAD_add]
+	@DEAD_Id nvarchar(50),
+	@DEAD_Name nvarchar(50),
+	@DEAD_Price nvarchar(50)
+AS
+begin
+insert into [DEAD_TABLE]
+(DEAD_Id,DEAD_Name,DEAD_Price)
+values
+(@DEAD_Id,@DEAD_Name,@DEAD_Price)
+
+
+
+end
